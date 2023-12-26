bac4d969352d8fdd23fac5855002ae9fd9f36cf5
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2023-12-26 23:22:07 +0530

    fixed js file

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const btn = document.querySelector(".btn");
+const inpContainer = document.querySelectorAll(".input-container");
+
 btn.addEventListener("click", () => {
     if (btn.classList.toString().indexOf("state-default") == -1) return;
     try {
         btn.classList.remove("state-default");
         btn.classList.add("state-loading");
+
+        //making input boxs disable
+        inpContainer.forEach((e) => {
+            e.classList.remove("state-default");
+            e.classList.add("state-disable");
+            e.querySelector("input").disabled = true;
+        })
+
         setTimeout(() => {
             btn.classList.remove("state-loading");
             btn.classList.add("state-disable");
@@ -11,4 +21,12 @@ btn.addEventListener("click", () => {
     } catch (err) {
         console.log("An Error Occured: " + err);
     }
+})
+
+
+
+inpContainer.forEach((e)=>{
+    e.addEventListener("mousemove",()=>{
+        e.querySelector("input").focus();
+    })
 })
